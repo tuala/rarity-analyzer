63f7b02fa951baf4e58f6fec2f0effcb49b121e3
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 00:58:52 +0100

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -37,11 +37,19 @@ const Header = () => {
 						</a>
 						<a
 							className=" hidden lg:block inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 dark:hover:bg-blue-500 ease-in-out duration-200"
-							href={process.env.NEXT_PUBLIC_OPENSEA}
+							href={process.env.NEXT_PUBLIC_TOFU}
 							target="_blank"
 							rel="noopener noreferrer nofollow"
 						>
-							View collection
+							View on Tofu NFT
+						</a>
+						<a
+							className=" hidden lg:block inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 dark:hover:bg-blue-500 ease-in-out duration-200"
+							href={process.env.NEXT_PUBLIC_AREAX}
+							target="_blank"
+							rel="noopener noreferrer nofollow"
+						>
+							View on AreaX NFT
 						</a>
 						<DarkMode />
 					</div>
