475381c67a8f272e770fbc7025e314fea1565c66
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 13:37:13 +0100

    Update Pagination.tsx

--- components/RarityList/Pagination.tsx
+++ components/RarityList/Pagination.tsx
@@ -58,7 +58,7 @@ const Pagination = ({
 							key={`page-${page}`}
 							className={`w-10 al relative block py-2 px-1 leading-tight bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-500 rounded cursor-pointer ml-2 flex items-center ${
 								filter.page === page
-									? "bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-700 text-white hover:bg-blue-700"
+									? "bg-amber-400 dark:bg-amber-400 dark:hover:bg-amber-200 text-white hover:bg-amber-200"
 									: ""
 							} `}
 							onClick={() => setFilter({ ...filter, page })}
