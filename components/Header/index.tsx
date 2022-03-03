707b8c21bd5b669b8649bfffebbffb23442b2fa9
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 02:04:14 +0100

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -36,7 +36,7 @@ const Header = () => {
 							Website
 						</a>
 						<a
-							className=" hidden lg:block inline-flex justify-center items-center px-4 py-2 rounded-md bg-blue-600 text-white font-medium transition-all hover:bg-blue-800 dark:hover:bg-blue-500 ease-in-out duration-200"
+							className=" hidden lg:block inline-flex justify-center items-center px-4 py-2 rounded-md bg-amber-400 text-white font-medium transition-all hover:bg-amber-200 dark:hover:bg-amber-200 ease-in-out duration-200"
 							href={process.env.NEXT_PUBLIC_TOFU}
 							target="_blank"
 							rel="noopener noreferrer nofollow"
