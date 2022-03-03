c6011b59132b252a4620360f1725658dbfd2ca9a
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 01:00:13 +0100

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,24 +2,17 @@ import * as React from "react";
 import Link from "next/link";
 import DarkMode from "../DarkMode";
 
-const Header = () => {
+const Footer = () => {
 	return (
 		<div className="dark:bg-gray-900 bg-gray-50">
 			<footer className="max-w-screen-xl w-full mx-auto xl:px-0 px-5">
 				<div className="space-x-6 py-6 text-xs w-full flex items-center md:justify-end justify-center text-gray-500 dark:text-gray-300">
 					<div>
 						Made by the{" "}
-						<a href="https://punkscape.xyz/" target="_blank" rel="noreferrer">
-							PunkScape community
+						<a href="https://www.tsquirrel.io/" target="_blank" rel="noreferrer">
+							TSquirrel 
 						</a>
 					</div>
-					<a
-						href="https://github.com/middlerange/rarity-analyzer"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Github
-					</a>
 					<div className="flex items-center justify-center space-x-1">
 						<DarkMode />
 					</div>
@@ -28,4 +21,4 @@ const Header = () => {
 		</div>
 	);
 };
-export default Header;
+export default Footer;
