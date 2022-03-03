5c9d07494af473915806b36e1c53f005546a02bd
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 01:39:19 +0100

    Update MarketPlaceLink.tsx

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Image from "next/image";
-import { OpenSeaIcon } from "../Icons";
+import { TofuIcon, AreaXIcon } from "../Icons";
 
 const MarketPlaceLinks = ({ id }: { id: string }) => {
 	return (
@@ -9,15 +9,15 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 				<>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://opensea.io/assets/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
+						href={`https://tofunft.com/nft/telos/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
-								<OpenSeaIcon />
+								<TofuIcon />
 							</span>
-							OpenSea
+							Tofu
 						</div>
 					</a>
 					<a
@@ -28,26 +28,9 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 					>
 						<div className="flex items-center">
 							<span className="mr-2">
-								<Image src="/rarible.png" width="20px" height="20px" />
+								<AreaXIcon />
 							</span>
-							Rarible
-						</div>
-					</a>
-					<a
-						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://etherscan.io/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}?a=${id}`}
-						target="_blank"
-						rel="noopener noreferrer nofollow"
-					>
-						<div className="flex items-center">
-							<span className="mr-2">
-								<Image
-									src="/etherscan-logo-circle.png"
-									width="20px"
-									height="20px"
-								/>
-							</span>
-							Etherscan
+							AreaXNFT
 						</div>
 					</a>
 				</>
