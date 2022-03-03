ea8aab7e73b4a904efe2e4d060130871ece295a8
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 11:22:08 +0100

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -19,7 +19,7 @@ const Header = () => {
 							{!process.env.NEXT_PUBLIC_COLLECTION_LOGO && (
 								<span className="sm:inline-block hidden text-lg font-semibold dark:text-gray-200">
 									{process.env.NEXT_PUBLIC_SITENAME || "site name"} <br />
-									<small class="text-md">{process.env.NEXT_COLLECTION_NAME}</small>
+									<small className="text-md">{process.env.NEXT_COLLECTION_NAME}</small>
 								</span>
 							)}
 						</a>
