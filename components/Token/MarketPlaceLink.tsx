ddb599a45ea526f7684cb9de9d1436fe0bc5d6c6
Thomas Cuvillier <titiomm@gmail.com>
2022-03-03 01:53:25 +0100

    Update MarketPlaceLink.tsx

--- components/Token/MarketPlaceLink.tsx
+++ components/Token/MarketPlaceLink.tsx
@@ -22,7 +22,7 @@ const MarketPlaceLinks = ({ id }: { id: string }) => {
 					</a>
 					<a
 						className="text-gray-600 dark:text-gray-300 flex flex-row items-center rounded-md hover:bg-gray-100  dark:hover:bg-gray-700 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-600 px-3 py-1.5 border transition-colors text-xs xs:text-base ease-in-out opacity-90 shadow-sm"
-						href={`https://rarible.com/token/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}:${id}`}
+						href={`https://evm.areaxnft.com/nft/${process.env.NEXT_PUBLIC_COLLECTION_TOKEN}/${id}`}
 						target="_blank"
 						rel="noopener noreferrer nofollow"
 					>
